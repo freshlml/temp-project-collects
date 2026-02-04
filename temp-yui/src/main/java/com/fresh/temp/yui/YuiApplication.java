acc23b5f1d63be9d70e7cc53364f103a37afedc2
freshlml <1326191992@qq.com>
2026-02-05 02:48:38 +0800

    1

--- temp-yui/src/main/java/com/fresh/temp/yui/YuiApplication.java
+++ temp-yui/src/main/java/com/fresh/temp/yui/YuiApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan({"com.fresh.temp.yui", "com.fresh.xy.common"})
+@ComponentScan({"com.fresh.temp.yui", "com.fresh.xy.common.exception"})
 public class YuiApplication {
 
     public static void main(String[] args) {
